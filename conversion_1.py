322b8d4cb50ae3625fc9466f449408e7d9eac5f5
sgoes <51802504+sgoes@users.noreply.github.com>
2021-03-02 12:26:47 +0000

    convert string to int

---
+++ conversion_1.py
@@ -0,0 +1,11 @@
+#Conversion_1
+#Print two digit numbers
+
+
+two_digit_number=input("type two digit: ")
+first_digit = two_digit_number [0]
+second_digit = two_digit_number [1]
+
+result = (int(first_digit) + int(second_digit))
+
+print(result)
