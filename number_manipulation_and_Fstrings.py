4034a0e95cb41a2e579332e284ce088b80a04ed7
sgoes <51802504+sgoes@users.noreply.github.com>
2021-03-02 16:49:48 +0000

    Create number_manipulation_and_Fstrings.py

---
+++ number_manipulation_and_Fstrings.py
@@ -0,0 +1,29 @@
+
+print(8 / 3)
+print (int(8 / 3))
+print(round(8 / 3))
+print(round(8 / 3, 2))
+print(type(8 // 3))
+print(type(8 / 3))
+print(8 // 3)
+print(8 / 3)
+
+result = 4 / 2
+result /= 2
+print(result)
+
+# user scores a point
+score = 0
+
+#score = score + 1
+score += 1
+print(score)
+print ("your score is " + str(score))
+
+#mix int, float and boolean
+score = 0
+height = 1.80
+isWinning = True
+
+#f-String
+print(f"your score is {score}, your height is {height}, you are winning is {isWinning}")
