157304222fffee1faccffc06df4d50fc1a7ef112
sgoes <51802504+sgoes@users.noreply.github.com>
2021-03-03 09:56:50 +0000

    Create if_else_rollercoaster.py

---
+++ if_else_rollercoaster.py
@@ -0,0 +1,11 @@
+#estruturas de decisão simples
+
+#IF AND ELSE
+
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in cm? "))
+
+if height >= 120:
+  print("you can ride the rollercoaster")
+else:
+  print("Sorry, you have to grow taller before you can ride.")
