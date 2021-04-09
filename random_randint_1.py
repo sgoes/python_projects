4dd0896cb21d8d9b46ead1bdd3494d2d5ec26430
sgoes <51802504+sgoes@users.noreply.github.com>
2021-04-09 22:37:03 +0100

    Update random_randint_1.py

--- random_randint_1.py
+++ random_randint_1.py
@@ -11,3 +11,10 @@ print(random_float)
 
 love_score = random.randint(1, 100)
 print(f"your love score is {love_score}")
+
+
+random_side = random.randint(0, 1)
+if random_side == 1:
+  print("Heads")
+else:
+  print("Tails")
