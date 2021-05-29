3deb018628195e277f771598cd360736f67c36d2
sgoes <51802504+sgoes@users.noreply.github.com>
2021-05-29 12:16:26 +0100

    Create loops_without_lists.py

---
+++ loops_without_lists.py
@@ -0,0 +1,12 @@
+#Write your code below this row 👇
+
+total = 0
+for number in range(2, 101, 2):
+  total += number
+  print(total)
+
+total2 = 0
+for number in range (1, 101):
+  if number % 2 == 0:
+    total += number
+print(total2)
