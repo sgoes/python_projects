33786258d949fc7c1ad6c8126ca7f11a0f478c82
Carolina Goes <51802504+sgoes@users.noreply.github.com>
2022-01-31 19:58:37 +0000

    Update 02_tipCalculator.py
    another way to solve this problem

--- 02_tipCalculator.py
+++ 02_tipCalculator.py
@@ -21,3 +21,16 @@ final_amounth = round(bill_per_person, 2)
 final_amounth = "{:.2f}".format(bill_per_person)
 
 print(f"Each person should pay {final_amounth}")
+
+
+
+#ohter way to do the things 
+total_bill = float(input("What was the total bill? "))
+tip = int(input("What percentage tip would you like to give? 10, 12 or 15: "))
+each_person = int(input("How many people who the split the bill? "))
+
+tip_as_percentage = tip /100
+total_bill_tip = total_bill + tip_as_percentage
+total = total_bill_tip / each_person
+
+print(f'Each person shloud pay {round(total, 2)}')
