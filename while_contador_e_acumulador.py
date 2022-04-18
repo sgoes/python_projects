53b002178ad8cd4d323e55f5be7e5109fdecd1ea
Carolina Goes <51802504+sgoes@users.noreply.github.com>
2022-04-18 16:29:23 +0100

    Create while_contador_e_acumulador.py

---
+++ while_contador_e_acumulador.py
@@ -0,0 +1,10 @@
+contador = 1
+acumulador = 1
+
+while contador <= 10:
+    print(contador, acumulador)
+
+    acumulador = acumulador + contador
+    contador += 1
+else:
+    print("Cheguei no else")
