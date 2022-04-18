dbf9f873f9ca49c474f6f99ca8de8e013da949b7
Carolina Goes <51802504+sgoes@users.noreply.github.com>
2022-04-18 14:33:36 +0100

    Create calculator_while_cicle.py

---
+++ calculator_while_cicle.py
@@ -0,0 +1,25 @@
+while True:
+    print()
+    num_1 = input('Digite um número: ')
+    num_2 = input('Digite outro número: ')
+    operador = input('Digite um operador: ')
+   
+    
+    if not num_1.isnumeric() or not num_2.isnumeric():
+        print('Precisa de digitar um número inteiro.')
+        continue
+
+    num_1 = int(num_1)
+    num_2 = int(num_2)        
+
+    # + - / *
+    if operador == '+':
+        print(num_1 + num_2)
+    elif operador == '-':
+        print(num_1 - num_2)
+    elif operador == '/':
+        print(num_1 / num_2)
+    elif operador == '*':
+        print(num_1 * num_2)
+    else:
+        print('Operador invalido')
