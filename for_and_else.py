7a5a810be7c9e4dcf1677c7028af7b4531dc7037
Carolina Goes <51802504+sgoes@users.noreply.github.com>
2022-06-24 13:25:35 +0100

    Create for_and_else.py

---
+++ for_and_else.py
@@ -0,0 +1,11 @@
+'''
+For / Else em Python
+'''
+
+variavel = ['Carolina Goes', 'Joaozinho', 'Maria']
+
+for valor in variavel:
+    if valor.startswith('J'):
+        print('Começa com J', valor)
+    else:
+        print('Não começa com J', valor)
